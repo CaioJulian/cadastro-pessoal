a1dc5ea3846dab83e9cba0f0df04cb2739ef6920
caiojulian <caiojulian@hotmail.com>
2018-12-26 13:12:39 -0400

    layout CardMenuVue

---
+++ src/components/layouts/CardMenuVue.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card" style="width: 18rem;">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GridVue',
+  props:[],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
